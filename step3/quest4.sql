f26827de0befe269b3b40d5e887ab1f63960022f
にっしー <nissi1270561@gmail.com>
2025-05-09 23:13:53 +0900

    fix: step4indexが有効になるようにクエリを修正

--- step3/quest4.sql
+++ step3/quest4.sql
@@ -2,10 +2,12 @@
 ドラマのチャンネルに対して、放送開始時刻、放送終了時刻、シーズン数、エピソード数、エピソードタイトル、エピソード詳細を本日から一週間分取得してください*/
 
 --ドラマチャンネルはchannel_idを1とする。
+--start_dateが7日後の23:59:59以内のカラムを対象とする。
 SELECT ss.start_time,ss.end_time,se.season_number,ep.episode_number,ep.episode_title,ep.episode_detail
 FROM       show_schedules AS ss
 INNER JOIN episode AS ep ON ep.episode_id = ss.episode_id
 INNER JOIN season  AS se ON se.season_id  = ep.season_id
 WHERE      ss.channel_id = 1
-AND        DATE(ss.start_time) BETWEEN CURDATE() AND DATE_ADD(CURDATE(),INTERVAL 7 DAY )
+AND        ss.start_time >= CURDATE() 
+AND        ss.start_time < CURDATE() + INTERVAL 8 DAY
 ORDER BY ss.start_time;
